cb9eaca5a6efacc08f2deb189080f0147e2443d6
hueychopper <codelearn370@gmail.com>
2024-03-24 11:25:21 -0400

    user-specific post deletion; user-specific avatars

--- fdr-test/src/components/Clubs.js
+++ fdr-test/src/components/Clubs.js
@@ -10,6 +10,7 @@ import "./styles/profiles.css";
 import { FaPlus } from "react-icons/fa";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { GoKebabHorizontal } from "react-icons/go";
+import { FaEllipsisH } from "react-icons/fa";
 
 const storage = getStorage(getApp(), "gs://web-fdr-notification.appspot.com");
 let clubo = "";
@@ -85,6 +86,8 @@ export default function Clubs() {
                     selposts[i].push(
                         {
                             "author": ctxprops.username,
+                            "author_id": ctxprops.id,
+                            "author_pfp": ctxprops.pfp,
                             "date": `${current_date.getFullYear()}-${(current_date.getMonth()+1) < 10 ? "0"+(current_date.getMonth()+1).toString() : current_date.getMonth()}-${current_date.getDate()}`,
                             "img": img,
                             "text": contentRef.current.value,
@@ -109,7 +112,6 @@ export default function Clubs() {
         console.log("edit the stuff i guess");
     }
     async function deletePost(postId) { //e.g postid-math:0,0
-        console.log(selposts);
         const club_name = postId.substring(7,postId.indexOf(":"));
         const club_index = postId.substring(postId.indexOf(":")+1, postId.indexOf(","));
         const inner_index = postId.substring(postId.indexOf(",")+1);
@@ -125,6 +127,7 @@ export default function Clubs() {
     }, []);
     const [toggle, setToggle] = useState("");
     const indToggle = (e) => {
+        // console.log(e.target.id);
         if(toggle === e.target.id) {
             setToggle("");
         } else {
@@ -135,35 +138,28 @@ export default function Clubs() {
 
     return (
         <div className="clubs-page">
-         
+            <button className="btn btnpost" data-toggle="modal" data-target="#makepost">Make new Post</button>
             <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"/>
+            <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+            <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"/>
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
-        
-<nav class="navbar navbar-light bg-white">
-        <a href="#" class="navbar-brand">Bootsbook</a>
-        <form class="form-inline">
-            <div class="input-group">
-                <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2"/>
-                <div class="input-group-append">
-                    <button class="btn btn-outline-primary" type="button" id="button-addon2">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </div>
-            </div>
-        </form>
-    </nav>
+            <nav class="navbar navbar-light bg-white">
+                    <a href="#" class="navbar-brand">Bootsbook</a>
+                    <form class="form-inline">
+                        <div class="input-group">
+                            <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-primary" type="button" id="button-addon2">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </nav>
 
 
     <div class="container-fluid gedf-wrapper">
@@ -190,65 +186,42 @@ export default function Clubs() {
                     </ul>
                 </div>
             </div>
-            <div class="col-md-6 gedf-main">
-
-             
-               
-
-
-
-           
-                
-                          
+            <div class="col-md-6 gedf-main">     
                           
             {selposts.map((post_arr, index) => {
                 return ( // bruh react be like...
                     <div key={index}>
                     {post_arr.map((post, i) => (
-                       <div class="card gedf-card">
-                       <div class="card-header">
-                           <div class="d-flex justify-content-between align-items-center">
-                        <div id={"postid-"+index.toString()+":"+i.toString()} key={i}class="d-flex justify-content-between align-items-center">
-                            {ctxprops.role === "site_admin" ? 
-                            <div>
-                                {/* <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</button> */}
+                       <div className="card gedf-card">
+                       <div className="card-header">
+                           <div className="d-flex justify-content-between align-items-center">
+                        <div id={"postid-"+index.toString()+":"+i.toString()} key={i} className="d-flex justify-content-between align-items-center">
+                            {ctxprops.role === "site_admin" || ctxprops.id === post.author_id ? 
+                            <div className="dropdown">
+                                <button className="btn btn-link dropdown-toggle" id={"btnid-"+index.toString()+":"+i.toString()} onClick={indToggle} > {/*gedf-drop1  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"*/}
+                                    <FaEllipsisH className="svg-menu" id={"btnid-"+index.toString()+":"+i.toString()} onClick={indToggle} />
+                                </button>
                                 {toggle === ("btnid-"+index.toString()+":"+i.toString()) ? 
-                                <div>
-                                    <p>this is button</p>
+                                <div className="dropdown dropdown-menu-right">
+                                    <div className="h6 dropdown-header">Configuration</div>
+                                    <button className="dropdown-item" onClick={editPost}>Edit</button>
+                                    <button className="warning-hover" onClick={()=>deletePost("postid-"+post.from_club.toString()+":"+index.toString()+","+i.toString())}>Delete</button>
                                 </div>
                                 : null}
                             </div>
                             : null}
                             <div class="mr-2">
-                                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt=""/>
-                                </div>
-                            <div class="ml-2">
-                                
+                                <img className="rounded-circle" width="45" src={post.author_pfp} alt=""/>
+                            </div>
+                            <div class="ml-2">  
                             <div class="h5 m-0">{post.author}</div>
                                     <div class="h7 text-muted">From {post.from_club} club</div>
-                
-                            </div>
-                               
-                        </div>
-                        <div>
-                                <div class="dropdown">
-                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <i class="fa fa-ellipsis-h"></i>
-                                    </button>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
-                                        <div class="h6 dropdown-header">Configuration</div>
-                                        <a class="dropdown-item" href="#"onClick={editPost}>Edit</a>
-                                        <a class="dropdown-item" href="#"onClick={()=>deletePost("postid-"+post.from_club.toString()+":"+index.toString()+","+i.toString())}>Delete</a>
-                                    
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                        
+                        </div>
             </div>
             <div class="card-body">
                         <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>{post.date}
-                       
                                      {post.type === "challenge" ?
                                      <p>due date: {post.due_date}</p> 
                                      :null}</div>
@@ -258,9 +231,6 @@ export default function Clubs() {
                              {post.img && post.img !== "img/img" ? 
                                      <img src={post.img} className="imgofpost" />
                                  : null}
-                                     
-                       
-
                         <p class="card-text">
                         {post.text}
                         </p>
@@ -270,20 +240,11 @@ export default function Clubs() {
                         <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
                         <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
                     </div>
-            </div>
-                        
+            </div>   
                     ))}
-                    
                     </div>
                 )
             })}
-            
-     
-
-
-           
-
-
                 <div class="card gedf-card">
                     <div class="card-header">
                         <div class="d-flex justify-content-between align-items-center">
@@ -328,12 +289,6 @@ export default function Clubs() {
                         <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
                     </div>
                 </div>
-              
-              
-
-
-
-
             </div>
             <div class="col-md-3">
                 <div class="card gedf-card">
@@ -360,10 +315,6 @@ export default function Clubs() {
         </div>
     </div>
             
-
-
-
-            <button data-toggle="modal" data-target="#makepost">Make new Post</button>
             <div className="override-flex">
             <div id="makepost" className="modal" tabIndex="-1" role="dialog">
             <div className="modal-dialog modal-dialog-centered" role="document">
