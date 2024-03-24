623f60c287754e30b0c93edaecdf441766a2b96d
atai20 <atai2005@gmail.com>
2024-03-24 02:20:18 -0400

    clubs design(buttons done not avatars yet)

--- fdr-test/src/components/Clubs.js
+++ fdr-test/src/components/Clubs.js
@@ -135,6 +135,234 @@ export default function Clubs() {
 
     return (
         <div className="clubs-page">
+         
+            <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+        crossorigin="anonymous"/>
+        
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+        
+<nav class="navbar navbar-light bg-white">
+        <a href="#" class="navbar-brand">Bootsbook</a>
+        <form class="form-inline">
+            <div class="input-group">
+                <input type="text" class="form-control" aria-label="Recipient's username" aria-describedby="button-addon2"/>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-primary" type="button" id="button-addon2">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+    </nav>
+
+
+    <div class="container-fluid gedf-wrapper">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="h5">@LeeCross</div>
+                        <div class="h7 text-muted">Fullname : Miracles Lee Cross</div>
+                        <div class="h7">Developer of web applications, JavaScript, PHP, Java, Python, Ruby, Java, Node.js,
+                            etc.
+                        </div>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <div class="h6 text-muted">Followers</div>
+                            <div class="h5">5.2342</div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="h6 text-muted">Following</div>
+                            <div class="h5">6758</div>
+                        </li>
+                        <li class="list-group-item">Vestibulum at eros</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-6 gedf-main">
+
+             
+               
+
+
+
+           
+                
+                          
+                          
+            {selposts.map((post_arr, index) => {
+                return ( // bruh react be like...
+                    <div key={index}>
+                    {post_arr.map((post, i) => (
+                       <div class="card gedf-card">
+                       <div class="card-header">
+                           <div class="d-flex justify-content-between align-items-center">
+                        <div id={"postid-"+index.toString()+":"+i.toString()} key={i}class="d-flex justify-content-between align-items-center">
+                            {ctxprops.role === "site_admin" ? 
+                            <div>
+                                {/* <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</button> */}
+                                {toggle === ("btnid-"+index.toString()+":"+i.toString()) ? 
+                                <div>
+                                    <p>this is button</p>
+                                </div>
+                                : null}
+                            </div>
+                            : null}
+                            <div class="mr-2">
+                                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt=""/>
+                                </div>
+                            <div class="ml-2">
+                                
+                            <div class="h5 m-0">{post.author}</div>
+                                    <div class="h7 text-muted">From {post.from_club} club</div>
+                
+                            </div>
+                               
+                        </div>
+                        <div>
+                                <div class="dropdown">
+                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                        <div class="h6 dropdown-header">Configuration</div>
+                                        <a class="dropdown-item" href="#"onClick={editPost}>Edit</a>
+                                        <a class="dropdown-item" href="#"onClick={()=>deletePost("postid-"+post.from_club.toString()+":"+index.toString()+","+i.toString())}>Delete</a>
+                                    
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+            </div>
+            <div class="card-body">
+                        <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>{post.date}
+                       
+                                     {post.type === "challenge" ?
+                                     <p>due date: {post.due_date}</p> 
+                                     :null}</div>
+                                      <a class="card-link" href="#">
+                            <h5 class="card-title">{post.title}</h5>
+                        </a>
+                             {post.img && post.img !== "img/img" ? 
+                                     <img src={post.img} className="imgofpost" />
+                                 : null}
+                                     
+                       
+
+                        <p class="card-text">
+                        {post.text}
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
+                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
+                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
+                    </div>
+            </div>
+                        
+                    ))}
+                    
+                    </div>
+                )
+            })}
+            
+     
+
+
+           
+
+
+                <div class="card gedf-card">
+                    <div class="card-header">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="mr-2">
+                                    <img class="rounded-circle" width="45" src="https://picsum.photos/50/50" alt=""/>
+                                </div>
+                                <div class="ml-2">
+                                    <div class="h5 m-0">@LeeCross</div>
+                                    <div class="h7 text-muted">Miracles Lee Cross</div>
+                                </div>
+                            </div>
+                            <div>
+                                <div class="dropdown">
+                                    <button class="btn btn-link dropdown-toggle" type="button" id="gedf-drop1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fa fa-ellipsis-h"></i>
+                                    </button>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="gedf-drop1">
+                                        <div class="h6 dropdown-header">Configuration</div>
+                                        <a class="dropdown-item" href="#">Save</a>
+                                        <a class="dropdown-item" href="#">Hide</a>
+                                        <a class="dropdown-item" href="#">Report</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="text-muted h7 mb-2"> <i class="fa fa-clock-o"></i>10 min ago</div>
+                        <a class="card-link" href="#">
+                            <h5 class="card-title">That's how it all should look like</h5>
+                        </a>
+
+                        <p class="card-text">
+                            Yep exactly like that
+                        </p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="#" class="card-link"><i class="fa fa-gittip"></i> Like</a>
+                        <a href="#" class="card-link"><i class="fa fa-comment"></i> Comment</a>
+                        <a href="#" class="card-link"><i class="fa fa-mail-forward"></i> Share</a>
+                    </div>
+                </div>
+              
+              
+
+
+
+
+            </div>
+            <div class="col-md-3">
+                <div class="card gedf-card">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <div class="card gedf-card">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+            
+
+
+
             <button data-toggle="modal" data-target="#makepost">Make new Post</button>
             <div className="override-flex">
             <div id="makepost" className="modal" tabIndex="-1" role="dialog">
@@ -173,59 +401,6 @@ export default function Clubs() {
             </div>
 
 
-            <div className="flexdisp">
-            {selposts.map((post_arr, index) => {
-                return ( // bruh react be like...
-                    <div key={index}>
-                        {post_arr.map((post, i) => {
-                            return (
-                                <div>
-                                {post.from_club !== undefined ?
-                                 <div className="standard-post" id={"postid-"+post.from_club.toString()+":"+index.toString()+","+i.toString()} key={i}>
-                                 <div className="btn-options"><GoKebabHorizontal id={"btnid-"+index.toString()+":"+i.toString()} onClick={indToggle} /></div>
-                                     {toggle === ("btnid-"+index.toString()+":"+i.toString()) ? 
-                                     <div className="dropdown-custom">
-                                         <button className="btn btnpost" onClick={editPost}>Edit post</button><br />
-                                         {ctxprops.role === "site_admin" ? 
-                                         <div>
-                                         <button className="btn btn-danger deletebtn" onClick={()=>deletePost("postid-"+post.from_club.toString()+":"+index.toString()+","+i.toString())}>Delete</button>
-                                         </div>
-                                     :null}
-                                     </div>
-                                     : null}
-                                 <p>from club: {post.from_club}</p>
-                                 <p>author: {post.author}</p>
-                                     <p>published: {post.date}</p>
-                                     {post.type === "challenge" ?
-                                     <p>due date: {post.due_date}</p> 
-                                     :null}
-                                 <p>title: {post.title}</p>
-                                 {post.img && post.img !== "img/img" ? 
-                                     <img src={post.img} className="imgofpost" />
-                                 : null}
-                                     <p>text: {post.text}</p>
-                             </div>
-                                : null}
-                                
-                                </div>
-                            )
-                        })}
-                    </div>
-                );
-            })}
-            
-            {challenge.map((c, i) => (
-                <div className="challenge" key={i}>
-                    <p>author: {c.author}</p>
-                    <p>text: {c.text}</p>
-                    {c.img !== "" && c.img !== null && c.img !== "img/img" ?
-                    <img src={c.img} width={300} height={200} />
-                    : null}
-                    <p>published: {c.date}</p>
-                    <p>due date: {c.due_date}</p>
-                </div>
-            ))}
-            </div>
         </div>
     )
 }
