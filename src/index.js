8b7beba6b5ce0b9d08bf5dcb656be2b897f5773a
codetomatot <codelearn370@gmail.com>
2024-04-09 11:38:57 -0400

    branchingfdfad

--- src/index.js
+++ src/index.js
@@ -59,7 +59,5 @@ const router = createBrowserRouter([
 ]);
 
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
