8e702ad2f4f4cf4ba308f54e36762ce98a0256de
atai20 <37374053+atai20@users.noreply.github.com>
2024-04-19 13:40:22 -0400

    Update index.js

--- fdr-test/src/index.js
+++ fdr-test/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 import ErrorRoute from './ErrorRoute';
 import Schools from "./components/Schools";
 import Landing from "./Landing";
@@ -9,9 +10,15 @@ import About from "./components/About";
 import Profile from "./components/Profile";
 import Clubs from "./components/Clubs";
 import Intro from "./components/Intro";
-import Cookie from "./components/Cookie";
+
+import Calendar from "./components/Scheduler";
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Scheduler from './components/Scheduler';
+import Resultsform from './components/Resultsform';
+import Schoolsform from './components/Schoolsform';
+import Clubform from './components/Clubform';
+import Newsform from './components/Newsform';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -38,12 +45,24 @@ const router = createBrowserRouter([
         element: <Clubs />,
       },
       {
-        path: "/calendar",
-        element: <Scheduler />,
+        path: "/formclubs",
+        element: <Clubform />,
       },
       {
-        path: "/cookie",
-        element: <Cookie />,
+        path: "/formnews",
+        element: <Newsform />,
+      },
+      {
+        path: "/formresults",
+        element: <Resultsform />,
+      },
+      {
+        path: "/formschools",
+        element: <Schoolsform />,
+      },
+      {
+        path: "/calendar",
+        element: <Scheduler />,
       },
       {
         path: "/about",
@@ -59,5 +78,12 @@ const router = createBrowserRouter([
 ]);
 
 root.render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
