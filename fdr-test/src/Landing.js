1b9b5392870a49281b8cebecba3fdc407f06e12c
atai20 <37374053+atai20@users.noreply.github.com>
2024-04-19 13:40:57 -0400

    Update Landing.js

--- fdr-test/src/Landing.js
+++ fdr-test/src/Landing.js
@@ -56,26 +56,7 @@ class Landing extends React.Component {
                         const [img, setImg] = useState([]);
                         const add_news_ref = useRef("");
                         const text_news_ref = useRef("");
-                      //   async function uploadImage(e) {
-                      //     for(let i = 0; i < e.target.files.length; i++) {
-                      //         const storageRef = ref(storage, `images/${ctxprops.id}/${e.target.files[i].name}`);
-                      //         const uploadTask = uploadBytesResumable(storageRef, e.target.files[i]);
-                      //         await uploadTask.on('state_changed', (snap) => {
-                      //             if(snap.state === "running") {
-                      //             console.log(snap.state);
-                      //             }
-                      //         }, (err) => {
-                      //             console.log("error upload");
-                      //         }, () => {
-                      //             getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-                      //                 imgs_t.push(url);
-                      //             })
-                      //         });
-                      //     }
-                      //     setImg(imgs_t);
-                      // }
-
-                        // console.log(outletCtxProps);
+                     
                         const add_news = async() => {
                             const docRef2 = await addDoc(collection(db, `schools/${outletCtxProps.school_select}/news`), {
                                 title: add_news_ref.current.value,
@@ -101,119 +82,14 @@ class Landing extends React.Component {
                         return (
                         
                         <div>
+<div className="center_news">
 <div class="d-flex">
- <div class="d-inline-block"><RiveComponent style={{height:"400px", width:"500px"}}/></div>
+ <div class="d-inline-block"><RiveComponent style={{height:"400px", width:"300px"}}/></div>
  <div class="d-inline-block"> <div class="chat">
     Hello! My name is Firey
     </div> </div>
-</div>
-{/* <form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Publish post or make a challenge</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="post description"/>
-      
-      <input type="checkbox" className="posttype_check" id="checker"/><label htmlFor="checker">challenge</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="due date *only for challenge"/>
     </div>
-</div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
     </div>
-  </div>
-  <button className="submit-info" id="submit-info" onClick={this.create_post}>Submit</button>
-</form>
-<h1>write results for week</h1>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-    </div>
-</div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Sign in</button>
-</form>
-<h1>Write results for month</h1>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Email</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="Email"/>
-    </div>
-</div>
-  <div class="form-group">
-    <div class="form-check">
-      <input class="form-check-input" type="checkbox" id="gridCheck"/>
-      <label class="form-check-label" for="gridCheck">
-        Check me out
-      </label>
-    </div>
-  </div>
-  <button type="submit" class="btn btn-primary" onClick={this.create_post}>Sign in</button>
-</form> */}
-
-
-{/* <h1>Add school</h1>
-<form>
-  <div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">school name</label>
-      <input type="email" class="form-control" ref={this.mynews_text} id="inputEmail4" placeholder="Email"/>
-    </div>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">description</label>
-      <input type="email" class="form-control" ref={this.mynews_text} id="inputEmail4" placeholder="Email"/>
-    </div>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">image</label>
-      <input type="file" class="form-control" ref={this.mynews_text} src="img/img.png"  id="inputEmail4" placeholder="Email"/>
-    </div>
-</div>
-
-  
-  <button class="btn btn-primary" onClick={this.add_news}>Sign in</button>
-</form>
-
-
-<h1>Give out talent points: 20</h1>
-<form>
-<div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Atai</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="number"/>
-    </div>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Giliana</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="number"/>
-    </div>
-</div>
-<div class="form-row">
-    <div class="form-group col-md-6">
-      <label for="inputEmail4">Sam</label>
-      <input type="email" class="form-control" id="inputEmail4" placeholder="number"/>
-    </div>
-</div>
-  
-  <button type="submit" class="btn btn-primary" onClick={this.create_post}>Sign in</button>
-</form> */}
 
                         </div>
                         );
