62275c7a0b1619415fad27856e9995cc2b69ab25
atai20 <ataikyd2005@gmail.com>
2024-04-23 14:39:31 -0400

    anime2

--- fdr-test/src/Landing.js
+++ fdr-test/src/Landing.js
@@ -3,7 +3,7 @@ import { Link, Outlet, useOutletContext } from "react-router-dom";
 import firebase from 'firebase/compat/app';
 import { getApp } from "firebase/app";
 import {getStorage, ref, uploadBytesResumable, getDownloadURL} from "firebase/storage";
-import { useRive } from '@rive-app/react-canvas';
+import Rive, { useRive } from '@rive-app/react-canvas';
 import { getFirestore, collection, getDocs, addDoc, query,getDoc, doc, setDoc, updateDoc, deleteDoc, onSnapshot, serverTimestamp } from "firebase/firestore";
 import {getAuth,signOut} from "firebase/auth";
 import {db} from "./firebase-config";
@@ -20,6 +20,9 @@ class Landing extends React.Component {
     constructor(props) {
         super(props);
         this.mynews_text = React.createRef();
+        this.state = {
+            showMe:true
+        }
     }
 
     
@@ -34,6 +37,9 @@ class Landing extends React.Component {
     componentDidMount() {
         // getDoc()
     }
+
+    
+   
     //Main feed page
     render() {
         return (
@@ -43,48 +49,20 @@ class Landing extends React.Component {
           
                 
                 <OutletProvider>
-                    {(outletCtxProps) => {
+                    {
+                    
+                    (outletCtxProps) => {
                         const [news_text, setNews] = useState([]);
                         let imgs_t = [];
                         let news_t = [];
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
                         const rive = useRive();
                        
                        
                     
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-                        const { rive, RiveComponent } = useRive({
-                          src: 'firey.riv',
-                          stateMachines: "State Machine 1",
-                        autoplay: true,
-                        });
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> 760ece13dd06d09404edd3925a2e25b81586cf33
->>>>>>> Stashed changes
-=======
->>>>>>> 760ece13dd06d09404edd3925a2e25b81586cf33
->>>>>>> Stashed changes
                         // const ctxprops = useOutletContext();
                         const [img, setImg] = useState([]);
                         const add_news_ref = useRef("");
                         const text_news_ref = useRef("");
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
                        
                         const  dissapear = async() => {
                             this.setState({
@@ -119,29 +97,6 @@ class Landing extends React.Component {
                                 showMe4:false,
                                 showMe_stud:true
                             })      
-=======
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-                     
-                        const add_news = async() => {
-                            const docRef2 = await addDoc(collection(db, `schools/${outletCtxProps.school_select}/news`), {
-                                title: add_news_ref.current.value,
-                              text: text_news_ref.current.value,
-                              img:img,
-                              date: serverTimestamp()
-                            });
-                            add_news_ref.current.value = "";
-                            text_news_ref.current.value = "";
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
->>>>>>> 760ece13dd06d09404edd3925a2e25b81586cf33
->>>>>>> Stashed changes
-=======
->>>>>>> 760ece13dd06d09404edd3925a2e25b81586cf33
->>>>>>> Stashed changes
                         }
                         const get_news = async() => {
                           const q = query(collection(db,`schools/${outletCtxProps.school_select}/news`));
@@ -159,15 +114,6 @@ class Landing extends React.Component {
                         
                         <div>
 <div className="center_news">
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<div class="d-flex">
- <div class="d-inline-block"><RiveComponent style={{height:"400px", width:"300px"}}/></div>
- <div class="d-inline-block"> <div class="chat">
-=======
-=======
->>>>>>> Stashed changes
-<<<<<<< HEAD
 
 
  
@@ -177,12 +123,65 @@ class Landing extends React.Component {
   <div class="d-inline-block"><Rive  src='firey.riv'ref={rive} style={{height:"400px", width:"300px"}}/></div>
  <div class="d-inline-block"> 
  <div class="chat"  onClick={() => dissapear()}  >
->>>>>>> Stashed changes
     Hello! My name is Firey
-    </div> </div>
+    </div> 
+    </div> 
+    </div> 
+
+   :null}
+   
+   {this.state.showMe2?
+ <div class="d-flex">
+
+    <div class="d-inline-block"><Rive src='firey_reg.riv' stateMachines='State Machine 1'style={{height:"400px", width:"300px"}}/></div>
+   <div class="d-inline-block"> 
+ <div class="chat"  onClick={() => dissapear2()}  >
+    I hope you will get an amazing experience!
+    </div> 
+    </div> 
+    </div> 
+   :null}
+    {this.state.showMe3?
+     <div class="d-flex">
+     <div class="d-inline-block"><Rive src='firey.riv' stateMachines='State Machine 1'style={{height:"400px", width:"300px"}}/></div>
+    <div class="d-inline-block"> 
+ <div class="chat"  onClick={() => dissapear3()}  >
+    Let's set everything up
+    </div> 
+    </div> 
+    </div> 
+
+   :null}
+    {this.state.showMe4?
+     <div class="d-flex">
+
+     <div class="d-inline-block"><Rive src='firey.riv' stateMachines='State Machine 1'style={{height:"400px", width:"300px"}}/></div>
+    <div class="d-inline-block"> 
+ <div class="chat"  >
+    First of all are you a teacher or a student?
+    </div> 
+       <button id="my_teacher_b"class="btn shadow-none" onClick={() => dissapear_teach()}  >teacher</button>
+    <button id="my_student_b"class="btn shadow-none" onClick={() => dissapear_stud()}  >student</button>
+    </div> 
+    </div> 
+   :null}
+
+{this.state.showMe_teach?
+     <div class="d-flex">
+
+     <div class="d-inline-block"><Rive src='firey_reg.riv' stateMachines='State Machine 3'style={{height:"400px", width:"300px"}}/></div>
+    <div class="d-inline-block"> 
+ <div class="chat">
+    For what clubs are you a teacher?
+    </div> 
+    <div className="clubs_but">
+    <button id="my_teacher_b"class="btn shadow-none" onClick={() => dissapear_teach()}  >Key Club</button>
+    <button id="my_student_b"class="btn shadow-none" onClick={() => dissapear_stud()}  >Robotics club</button>
+    <button id="my_teacher_b"class="btn shadow-none" onClick={() => dissapear_teach()}  >Key Club</button>
+    <button id="my_student_b"class="btn shadow-none" onClick={() => dissapear_stud()}  >Robotics club</button>
+    <button id="my_teacher_b"class="btn shadow-none" onClick={() => dissapear_teach()}  >Key Club</button>
+    <button id="my_student_b"class="btn shadow-none" onClick={() => dissapear_stud()}  >Robotics club</button>
     </div>
-<<<<<<< Updated upstream
-=======
     </div> 
     </div> 
    :null}
@@ -208,18 +207,6 @@ class Landing extends React.Component {
    :null}
   
     
-=======
-<div class="d-flex">
- <div class="d-inline-block"><RiveComponent style={{height:"400px", width:"300px"}}/></div>
- <div class="d-inline-block"> <div class="chat">
-    Hello! My name is Firey
-    </div> </div>
-    </div>
->>>>>>> 760ece13dd06d09404edd3925a2e25b81586cf33
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     </div>
 
                         </div>
